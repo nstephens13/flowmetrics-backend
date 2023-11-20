be816f01d54ea4899190f592dbca949e6efa8f79
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-11-20 21:03:49 +0100

    fix:#3 Remove unused import and add JSDoc comments to ChangeLogIF

--- src/model/ChangeLogIF.ts
+++ src/model/ChangeLogIF.ts
@@ -1,7 +1,11 @@
-import { EmployeeIF } from "./EmployeeIF";
 import { ChangeIF } from "./ChangeIF";
 
-
+/**
+ * 
+ * @prop {number} id the id of the changelog
+ * @prop {number} totalchanges the total number of changes
+ * @prop {ChangeIF[]} changes the list of changes
+ */
 export interface ChangeLogIF {
     id: number;
     totalchanges: number;
