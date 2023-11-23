aa949bb3a84d9e891fea16a340db02d95b25441b
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-11-23 11:38:41 +0100

    feat:#3 Refactor jiraResponseParser.ts

--- src/services/jiraResponseParser.ts
+++ src/services/jiraResponseParser.ts
@@ -1,19 +1,24 @@
-import { ChangeIF, ChangeType } from '../model/ChangeIF';
-import { ChangeLogIF } from '../model/ChangeLogIF';
-import type { EmployeeIF } from '../model/EmployeeIF';
-import type { IssueIF } from '../model/IssueIF';
+import { ChangeIF, ChangeType } from "../model/ChangeIF";
+import { ChangeLogIF } from "../model/ChangeLogIF";
+import type { EmployeeIF } from "../model/EmployeeIF";
+import type { IssueIF } from "../model/IssueIF";
 
 /**
  * @description function to extract the first and last name from the display name
- * 
+ *
  * @param displayName display name of the employee
  * @returns first and last name as an object
  */
-function extractNames(displayName: string): { firstName: string, lastName: string } {
-  const commaIndex = displayName.indexOf(',');
+function extractNames(displayName: string): {
+  firstName: string;
+  lastName: string;
+} {
+  const commaIndex = displayName.indexOf(",");
 
   if (commaIndex === -1) {
-    throw new Error('Invalid display name format. Expected format: "LastName, FirstName".');
+    throw new Error(
+      'Invalid display name format. Expected format: "LastName, FirstName".'
+    );
   }
 
   const lastName = displayName.slice(0, commaIndex).trim();
@@ -24,11 +29,11 @@ function extractNames(displayName: string): { firstName: string, lastName: strin
 
 /**
  * @description function to parse the employee to a EmployeeIF
- * 
+ *
  * @param employeeJSON parsed response from jira
  * @returns employee as a EmployeeIF or null if response is null
  */
-export function parseEmployee(employeeJSON: any) : EmployeeIF | null {
+export function parseEmployee(employeeJSON: any): EmployeeIF | null {
   if (employeeJSON == null) {
     return null;
   }
@@ -37,16 +42,16 @@ export function parseEmployee(employeeJSON: any) : EmployeeIF | null {
     id: employeeJSON.key,
     firstName,
     lastName,
-    emailAddress: 'none',
-    avatarUrl: 'none',
-    status: "inactive"
+    emailAddress: "none",
+    avatarUrl: "none",
+    status: "inactive",
   };
   return employee;
 }
 
 /**
  * @description function to parse the date to a Date
- * 
+ *
  * @param response parsed response from jira
  * @returns date as a Date or null if response is null
  */
@@ -59,11 +64,11 @@ export function parseDate(response: any): Date | null {
 
 /**
  * @description function to parse the issues to a IssueIF
- * 
+ *
  * @param response parsed response from jira
  * @returns issue as a IssueIF or null if response is null
  */
-export function parseIssue(response: any) : IssueIF | null {
+export function parseIssue(response: any): IssueIF | null {
   if (response == null) {
     return null;
   }
@@ -80,62 +85,71 @@ export function parseIssue(response: any) : IssueIF | null {
     statusChanges: null,
     lastStatusChange: parseDate(null),
     assignedSLARule: null,
-    changelog: parseChangeLog(response.changelog)
+    changelog: parseChangeLog(response.changelog),
   };
   return issue;
 }
 
 /**
  * @description function to parse the changelog to a ChangeLogIF[]
- * 
+ *
  * @param response parsed response from jira
  * @returns changelog as a ChangeLogIF[] or null if response is null
  */
-export function parseChangeLog(response: any) : ChangeLogIF[] | null {
+export function parseChangeLog(response: any): ChangeLogIF[] | null {
   if (response == null) {
     return null;
   }
-  //     const issues: IssueIF[] = response?.issues.map((issueJSON: any) => parseIssue(issueJSON));
-  const changeLogs: ChangeLogIF[] = response?.histories?.map((history: any) => parseChange(history)); 
+  const changeLogs: ChangeLogIF[] = response?.histories?.map((history: any) =>
+    {
+      if (history === null) {
+        return null;
+      }
+      let changes = parseChangeHistory(history.items);
+      if (changes?.length === 0) {
+        return null;
+      } else {
+        const change: ChangeLogIF = {
+          id: history.id,
+          created: parseDate(history.created),
+          author: parseEmployee(history.author),
+          changes: changes,
+        };
+        return change;
+      }
+    }
+  ).filter((changeLog: ChangeLogIF | null) => changeLog !== null);
   return changeLogs;
 }
 
 /**
- * @description function to parse the history from changelog to a ChangeLogIF
- * 
- * @param history history from changelog
- * @returns history as a ChangeLogIF or null if response is null
- */
-function parseChange(history: any) : ChangeLogIF | null {
-  if (history == null) {
-    return null;
-  }
-  const change: ChangeLogIF = {
-    id: history.id,
-    created: parseDate(history.created),
-    author: parseEmployee(history.author),
-    changes: parseChangeHistory(history.items)
-  }
-  return change;
-}
-
-/**
  * @description function to parse each item from history to a ChangeIF
- * 
+ *
  * @param items parsed items from history
  * @returns items as a ChangeIF[] or null if response is null
  */
 function parseChangeHistory(items: any): ChangeIF[] | null {
-  if (items == null) {
+  if (items === null) {
     return null;
   }
   const changeHistories: ChangeIF[] = items?.map((itemJSON: any) => {
-    const changeHistory: ChangeIF = {
-      changeType: itemJSON.field,
-      from: itemJSON.fromString,
-      to: itemJSON.toString
-    }
-    return changeHistory;
-  });
+      let changeType: ChangeType | null = null;
+      if (itemJSON.field === ChangeType.assigneeChange) {
+        changeType = ChangeType.assigneeChange;
+      } else if (itemJSON.field === ChangeType.statusChange) {
+        changeType = ChangeType.statusChange;
+      }
+      if (changeType === null) {
+        return null;
+      } else {
+        const changeHistory: ChangeIF = {
+          changeType,
+          from: itemJSON.fromString,
+          to: itemJSON.toString,
+        };
+        return changeHistory;
+      }
+    })
+    .filter((changeHistory: ChangeIF | null) => changeHistory !== null);
   return changeHistories;
 }
