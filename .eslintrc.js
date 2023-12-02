c998988050c8e11d90175cdb06002b74da4c512c
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-12-02 10:24:55 +0100

    style:#6 initial setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: [
-    'vue',
-  ],
-  rules: {
+  env: {
+    node: true,
   },
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:import/typescript', 'prettier'],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        '': 'never',
+      },
+    ],
+    '@typescript-eslint/indent': 0, // deactivates intend
+    '@typescript-eslint/no-unused-vars': 'error', // gives error if declared variable is not used
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }], // prohibits use of the ++ operator
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'warn',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }], // warns against the use of dependencies in certain files
+    'import/no-unresolved': 0, // checks import paths
+    '@typescript-eslint/no-explicit-any': 0, // warns against the use of 'any' types
+    'linebreak-style': ['error', 'unix'], // sets the desired end-of-line style
+    'new-cap': 0, // concerns the use of the constructor with 'new'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'warn', // warning for explicit use of 'any'
+        '@typescript-eslint/ban-types': 'warn', // warning for certain TypeScript types
+      },
+    },
+  ],
 };
