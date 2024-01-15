5f59cb2cb429124cbcc17e629b130c30b64b3e4e
Elmar Kresse <elmar.kresse@stud.htwk-leipzig.de>
2024-01-15 18:47:56 +0100

    fix:#10 linted and format, removed unused functions after extraction to backend

--- src/model/ProjectIF.ts
+++ src/model/ProjectIF.ts
@@ -1,4 +1,3 @@
-import { nonDisplayedStatusList, nonDisplayedStateList } from '@/services/Issue';
 import type { IssueIF } from './Issue/IssueIF';
 import type { SlaCustomerProject } from '@/model/Sla/SlaCustomerProject';
 
@@ -7,7 +6,6 @@ import type { SlaCustomerProject } from '@/model/Sla/SlaCustomerProject';
  * @prop {number} id project id
  * @prop {string} name the name of the project
  * @prop {string} description the description of the project
- * @prop {Milestone[]} milestones a array of Milestone - objects assigned to the project
  * @prop {Issue[]} issues a array of Issues - objects that are assigned to the project
  * but not to a milestone
  */
@@ -17,28 +15,4 @@ export interface ProjectIF {
   description: string;
   issues: IssueIF[];
   slaSubscriber: SlaCustomerProject | null;
-}
-
-export function getIssueStatusList(issues: IssueIF[]): string[] {
-  const statusList: string[] = Array.from(
-    new Set(
-      issues
-        .map((issue) => issue.status)
-        .filter((status) => status && !nonDisplayedStatusList.includes(status))
-        .filter((status): status is string => status !== null)
-    )
-  );
-  return statusList;
-}
-
-export function getIssueStateList(issues: IssueIF[]): string[] {
-  const stateList: string[] = Array.from(
-    new Set(
-      issues
-        .map((issue) => issue.state)
-        .filter((state) => state && !nonDisplayedStateList.includes(state))
-        .filter((state): state is string => state !== null)
-    )
-  );
-  return stateList;
 }
--- src/services/Issue.ts
+++ src/services/Issue.ts
@@ -36,8 +36,6 @@ class Issue implements IssueIF {
 
   state: string | null;
 
-  static planningStatusList: string[];
-
   constructor(
     id: number,
     name: string,
@@ -73,100 +71,5 @@ class Issue implements IssueIF {
   }
 }
 
-// Define lists of different category with statuses
-const planningStatusList: string[] = ['planned', 'design', 'open'];
-const devStatusList: string[] = ['in work', 'review', 'in progress'];
-const testingStatusList: string[] = ['unit test', 'e2e'];
-const nonDisplayedStatusList: string[] = ['closed'];
-const nonDisplayedStateList: string[] = [''];
-
-// Function sets State using Issue-Status
-function assignStateToIssue(issue: Issue): string | null {
-  const status = issue.status || '';
-
-  if (planningStatusList.includes(status)) {
-    return 'planning';
-  }
-  if (devStatusList.includes(status)) {
-    return 'development';
-  }
-  if (testingStatusList.includes(status)) {
-    return 'testing';
-  }
-  if (nonDisplayedStatusList.includes(status)) {
-    return null;
-  }
-  return 'undefined';
-}
-/**
- * Returns the name of the employee assigned to the issue.
- * @param issue - The Issue object.
- * @returns The assigned employee's name.
- */
-
-function getAssignedToName(issue: Issue): string {
-  if (issue.assignedTo) {
-    return `${issue.assignedTo.firstName} ${issue.assignedTo.lastName}`;
-  }
-  return '';
-}
-
-/**
- * Returns the formatted due date of the issue.
- * @param issue - The Issue object.
- * @returns The formatted due date string.
- */
-function getFormattedDate(issue: Issue): string {
-  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-  return issue.dueTo ? issue.dueTo.toLocaleDateString('en-US', options) : '';
-}
-
-/**
- * Returns the number of days left until the due date of the issue.
- * @param issue - The Issue object.
- * @returns The number of days left, or null if no due date is set.
- */
-function getTimeLeft(issue: Issue): number | null {
-  if (issue.dueTo) {
-    const currentTime = new Date().getTime();
-    const dueTime = issue.dueTo.getTime();
-    const timeLeft = Math.max(0, dueTime - currentTime);
-    return Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
-  }
-  return null;
-}
-
-/**
- * Counts the number of issues in the given issue list with the specified status.
- * If the status is null, counts all the issues in the list.
- * @param issueList - The list of issues.
- * @param status - The status of the issues to count.
- * @returns The count of issues.
- */
-function countIssuesByStatus(issueList: Issue[], status: string | null): number {
-  return (status ? issueList.filter((issue) => issue.status === status) : issueList).length;
-}
-
-/**
- * Returns SLA-Rules assigned to an Issue
- * @returns The Array of SLARules assigned to an Issue, can be null
- */
-function getSlaRules(issue: Issue) {
-  return issue.assignedSlaRule ?? [];
-}
-
 // export of data array and remain time for ticket calculation
-export {
-  Issue,
-  getTimeLeft,
-  getFormattedDate,
-  getAssignedToName,
-  countIssuesByStatus,
-  getSlaRules,
-  assignStateToIssue,
-  planningStatusList,
-  devStatusList,
-  testingStatusList,
-  nonDisplayedStatusList,
-  nonDisplayedStateList,
-};
+export default { Issue };
