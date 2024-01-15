82c6c505fd3b1dd0ba9be505d323acc5b076a82a
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2024-01-15 11:20:55 +0100

    fix:#9 SLARule to slaRule

--- src/model/__test__/SLARule.spec.ts
+++ src/model/__test__/SLARule.spec.ts
@@ -3,7 +3,7 @@ import { SLARule } from '../SLARule';
 
 describe('SLARule', () => {
   it('should have correct properties', () => {
-    const SLARule: SLARule = {
+    const slaRule: SLARule = {
       id: 1,
       name: 'SLA Rule 1',
       durationInDays: 1,
@@ -12,27 +12,27 @@ describe('SLARule', () => {
       occurredIn: 'open',
     };
 
-    expect(SLARule).toHaveProperty('id');
-    expect(typeof SLARule.id).toBe('number');
-    expect(SLARule.id).toBe(1);
+    expect(slaRule).toHaveProperty('id');
+    expect(typeof slaRule.id).toBe('number');
+    expect(slaRule.id).toBe(1);
 
-    expect(SLARule).toHaveProperty('name');
-    expect(typeof SLARule.name).toBe('string');
-    expect(SLARule.name).toBe('SLA Rule 1');
+    expect(slaRule).toHaveProperty('name');
+    expect(typeof slaRule.name).toBe('string');
+    expect(slaRule.name).toBe('SLA Rule 1');
 
-    expect(SLARule).toHaveProperty('durationInDays');
-    expect(typeof SLARule.durationInDays).toBe('number');
-    expect(SLARule.durationInDays).toBe(1);
+    expect(slaRule).toHaveProperty('durationInDays');
+    expect(typeof slaRule.durationInDays).toBe('number');
+    expect(slaRule.durationInDays).toBe(1);
 
-    expect(SLARule).toHaveProperty('expirationDate');
-    expect(typeof SLARule.expirationDate).toBe('object');
+    expect(slaRule).toHaveProperty('expirationDate');
+    expect(typeof slaRule.expirationDate).toBe('object');
 
-    expect(SLARule).toHaveProperty('maxAssignedEmployees');
-    expect(typeof SLARule.maxAssignedEmployees).toBe('number');
-    expect(SLARule.maxAssignedEmployees).toBe(5);
+    expect(slaRule).toHaveProperty('maxAssignedEmployees');
+    expect(typeof slaRule.maxAssignedEmployees).toBe('number');
+    expect(slaRule.maxAssignedEmployees).toBe(5);
 
-    expect(SLARule).toHaveProperty('occurredIn');
-    expect(typeof SLARule.occurredIn).toBe('string');
-    expect(SLARule.occurredIn).toBe('open');
+    expect(slaRule).toHaveProperty('occurredIn');
+    expect(typeof slaRule.occurredIn).toBe('string');
+    expect(slaRule.occurredIn).toBe('open');
   });
 });
