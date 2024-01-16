a8218bb4f841fb08e871f70e3088678f67b947bf
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2024-01-16 13:16:43 +0100

    fix:#9 linting errors

--- src/services/__test__/jiraRequestHelper.spec.ts
+++ src/services/__test__/jiraRequestHelper.spec.ts
@@ -5,30 +5,30 @@ import { fetchUserInfo } from '../jiraRequestHelper';
 jest.mock('axios');
 
 describe('jiraRequestHelper fetchUserInfo', () => {
-    it('fetchUserInfo makes request and returns correct properties', async () => {
-        // Mock axios.get function 
-        (axios.get as jest.Mock).mockResolvedValue({
-            data: {
-                id: expect(Number),
-                firstName: expect(String),
-                lastName: expect(String),
-                status: expect(String),
-                assignedIssues: expect(Array),
-                emailAddress: expect(String),
-                avatarUrl: expect(String),
-                key: expect(String),
-            }
-        });
-
-        const userInfo = await fetchUserInfo();
-
-        expect(userInfo).toHaveProperty('id');
-        expect(userInfo).toHaveProperty('firstName');
-        expect(userInfo).toHaveProperty('lastName');
-        expect(userInfo).toHaveProperty('status');
-        expect(userInfo).toHaveProperty('assignedIssues');
-        expect(userInfo).toHaveProperty('emailAddress');
-        expect(userInfo).toHaveProperty('avatarUrl');
-        expect(userInfo).toHaveProperty('key');
+  it('fetchUserInfo makes request and returns correct properties', async () => {
+    // Mock axios.get function
+    (axios.get as jest.Mock).mockResolvedValue({
+      data: {
+        id: expect(Number),
+        firstName: expect(String),
+        lastName: expect(String),
+        status: expect(String),
+        assignedIssues: expect(Array),
+        emailAddress: expect(String),
+        avatarUrl: expect(String),
+        key: expect(String),
+      },
     });
+
+    const userInfo = await fetchUserInfo();
+
+    expect(userInfo).toHaveProperty('id');
+    expect(userInfo).toHaveProperty('firstName');
+    expect(userInfo).toHaveProperty('lastName');
+    expect(userInfo).toHaveProperty('status');
+    expect(userInfo).toHaveProperty('assignedIssues');
+    expect(userInfo).toHaveProperty('emailAddress');
+    expect(userInfo).toHaveProperty('avatarUrl');
+    expect(userInfo).toHaveProperty('key');
+  });
 });
